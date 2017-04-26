b31a9dff1f9a8a8b5d1e35034888b0f0d801b72e
Brian Carroll <briancarroll@USCAL-300013-Mac.local>
2017-04-26 07:49:45 -0700

    Added logic to check http method

--- lambda/postImage.py
+++ lambda/postImage.py
@@ -9,24 +9,35 @@ def datetime_handler(x):
     if isinstance(x, datetime.datetime):
         return x.isoformat()
     raise TypeError("Unknown type")
-
-def response(body, status):
-    response = {
+    
+def responseBody(body, status):
+    return {
         "isBase64Encoded": "false",
         "statusCode": status,
-        "headers": { "Content-Type": "text/json", "Access-Control-Allow-Origin": "*" },
+        "headers": { "Access-Control-Allow-Origin": "*",
+                     "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"},
         "body": json.dumps(body, default=datetime_handler)
     }
-    return response
-
-def lambda_handler(event, context):
-    fileName = os.path.basename(event['url'])
-    file = urllib2.urlopen(event['url'])
+    
+def postImage(url):
+    fileName = os.path.basename(url)
+    file = urllib2.urlopen(url)
     fileBuffer = StringIO(file.read())
     s3 = boto3.client('s3')
     resp = s3.put_object(ACL="public-read",
                          Bucket="briancoxen.me",
                          Body=fileBuffer,
                          Key="photos/thumbnails/%s" % fileName);
-    
-    return response(resp, 200)
+    return resp
+
+def response(event):
+    if event['httpMethod'] == 'OPTIONS':
+        return responseBody({}, 200)
+    elif event['httpMethod'] == 'POST':
+        body = json.loads(event['body'])
+        return responseBody(postImage(body['url']), 200)
+    else:
+        return responseBody({}, 401)
+
+def lambda_handler(event, context):
+    return response(event)
